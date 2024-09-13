fcb86d3a1ea82c0d7caa18b210d53f5deb08284e
KOROyo123 <1914805725@qq.com>
2024-09-13 13:45:45 +0800

    更新windows的自动构建脚本

--- ci/build_in_windows.ps1
+++ ci/build_in_windows.ps1
@@ -9,4 +9,19 @@ Move-Item -Path "bin\Release\*" -Destination "bin"  -Force
 # 删除 build/release 目录
 Remove-Item -Path "bin\Release" -Recurse -Force
 
-Copy-Item -Path ".\.cmake\redis_windows.conf.in" -Destination ".\bin\env\redis.conf" -Force
+
+New-Item -Path "bin\env" -ItemType Directory
+
+Move-Item -Path "env\Redis-7.4.0-Windows-x64-msys2\*" -Destination "bin\env"  -Force
+
+Copy-Item -Path ".cmake\redis_windows.conf.in" -Destination "bin\env\redis.conf" -Force
+
+
+# 创建目标目录
+New-Item -Path "bin\env" -ItemType Directory -Force
+
+# 移动目录和文件
+Move-Item -Path "env\Redis-7.4.0-Windows-x64-msys2\*" -Destination "bin\env" -Force
+
+# 复制文件，并重命名
+Copy-Item -Path ".cmake\redis_windows.conf.in" -Destination "bin\env\redis.conf" -Force
