a0b42d3b9f070bd101b97ee38a681a0a750125e7
KOROyo123 <1914805725@qq.com>
2026-03-17 15:10:45 +0800

    尝试解决windows下action构建失败问题

--- ci/build_in_windows.ps1
+++ ci/build_in_windows.ps1
@@ -3,11 +3,11 @@ cmake -B build -DCMAKE_CXX_COMPILER=cl -DCMAKE_C_COMPILER=cl -DCMAKE_RUNTIME_OUT
 
 cmake --build build --config Release -- /m
 
-# # 移动所有文件和子目录从 build/release 到 build
-# Move-Item -Path "bin\Release\*" -Destination "bin"  -Force
+# 移动所有文件和子目录从 build/release 到 build
+Move-Item -Path "bin\Release\*" -Destination "bin"  -Force
 
-# # 删除 build/release 目录
-# Remove-Item -Path "bin\Release" -Recurse -Force
+# 删除 build/release 目录
+Remove-Item -Path "bin\Release" -Recurse -Force
 
 # 创建目标目录
 New-Item -Path "bin\env" -ItemType Directory -Force
